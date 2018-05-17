785b5cab6eab874735bbc0b31af0ea721b3a85c2
Nizar <me@nizar.se>
2018-05-17 16:47:25 +0200

    Added User Model

---
+++ public/src/app/user/user.model.ts
@@ -0,0 +1,44 @@
+// modelfor User
+export class User {
+    constructor(id: {
+        type: string,
+        required: true
+    },
+    login: {
+        type: string,
+        required: true
+    },
+    display_name: {
+        type: string,
+        required: true
+    },
+    type: {
+        type: string,
+        required: true
+    },
+    broadcaster_type: {
+        type: string,
+        required: true
+    },
+    description: {
+        type: string,
+        required: true
+    },
+    profile_image_url: {
+        type: string,
+        required: true
+    },
+    offline_image_url: {
+        type: string,
+        required: true
+    },
+    view_count: {
+        type: number,
+        required: true
+    },
+    email: {
+        type: string,
+        required: false
+    }) {
+    }
+}
