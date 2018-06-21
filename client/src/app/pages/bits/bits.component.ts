8b1d7e3e79b0a1d95f86d1f2cbed19442fc0809d
Nizars <me@nizar.se>
2018-06-21 17:21:16 +0200

    Bits Leaderboard styled and working

--- client/src/app/pages/bits/bits.component.ts
+++ client/src/app/pages/bits/bits.component.ts
@@ -2,7 +2,7 @@
  * @Author: Nizars
  * @Date: 2018-06-18 20:19:26
  * @Last Modified by: Nizars
- * @Last Modified time: 2018-06-20 23:57:10
+ * @Last Modified time: 2018-06-21 17:19:10
  */
 
 import { Component, OnInit } from '@angular/core';
@@ -43,13 +43,6 @@ export class BitsComponent implements OnInit {
   };
 
 
-  /**
-   * @name constructor
-   * @desc Creates an instance of BitsComponent.
-   * @constructor
-   * @param {TwitchApiService} twitchApi
-   * @memberof BitsComponent
-   */
   constructor(private twitchApi: TwitchApiService) { }
 
   ngOnInit() {
@@ -58,18 +51,26 @@ export class BitsComponent implements OnInit {
 
   // GET BIT LEADERBOARD
   getBitsLeaderboard() {
+
     const reqData = {
       count: this.resultsToReturn,
       period: this.resultsPeriod,
       started: this.dateTimeInRFC339(this.formatDate(), this.formatTime()),
       user_id: this.userId
     };
+
     this.twitchApi.getBitsLeaderboard(reqData)
     .subscribe(leaderboard => {
+
       this.leaderboardEntries = [];
       for (let i = 0; i < leaderboard.total; i++) {
-        this.leaderboardEntries.push(new Entry(leaderboard.data[i].user_id, leaderboard.data[i].rank, leaderboard.data[i].score));
+        this.leaderboardEntries.push(new Entry(
+          leaderboard.data[i].user_id,
+          leaderboard.data[i].rank,
+          leaderboard.data[i].score
+        ));
       }
+
       this.leaderboard = new BitsLeaderboard(
         this.leaderboardEntries,
         new DateRange(
@@ -78,100 +79,258 @@ export class BitsComponent implements OnInit {
         ),
         leaderboard.total
       );
+
       this.getLeaderboardUsers();
+
     });
   }
 
   // GET BIT LEADERBOARD USERS
   getLeaderboardUsers() {
     for (let i = 0; i < this.leaderboard.getTotal(); i++) {
+
       this.twitchApi.getUserById(this.leaderboardEntries[i].user_id)
       .subscribe(user => {
+
+        let id: string;
+        let login: string;
+        let display_name: string;
+        let type: string;
+        let broadcaster_type: string;
+        let description: string;
+        let profile_image_url: string;
+        let offline_image_url: string;
+        let view_count: string;
+
+        if ((user.data[0] !== null) &&
+            (user.data[0] !== undefined)) {
+
+          if ((user.data[0].id !== undefined) &&
+              (user.data[0].id !== undefined) ) {
+            id = user.data[0].id;
+          } else {
+            id = null;
+          }
+
+          if ((user.data[0].login !== undefined) &&
+              (user.data[0].login !== undefined) ) {
+            login = user.data[0].login;
+          } else {
+            login = null;
+          }
+
+          if ((user.data[0].display_name !== undefined) &&
+              (user.data[0].display_name !== undefined) ) {
+            display_name = user.data[0].display_name;
+          } else {
+            display_name = null;
+          }
+
+          if ((user.data[0].type !== undefined) &&
+              (user.data[0].type !== undefined) ) {
+            type = user.data[0].type;
+          } else {
+            type = null;
+          }
+
+          if ((user.data[0].broadcaster_type !== undefined) &&
+              (user.data[0].broadcaster_type !== undefined) ) {
+            broadcaster_type = user.data[0].broadcaster_type;
+          } else {
+            broadcaster_type = null;
+          }
+
+          if ((user.data[0].description !== undefined) &&
+              (user.data[0].description !== undefined) ) {
+            description = user.data[0].description;
+          } else {
+            description = null;
+          }
+
+          if ((user.data[0].profile_image_url !== undefined) &&
+              (user.data[0].profile_image_url !== undefined) ) {
+            profile_image_url = user.data[0].profile_image_url;
+          } else {
+            profile_image_url = null;
+          }
+
+          if ((user.data[0].offline_image_url !== undefined) &&
+              (user.data[0].offline_image_url !== undefined) ) {
+            offline_image_url = user.data[0].offline_image_url;
+          } else {
+            offline_image_url = null;
+          }
+
+          if ((user.data[0].view_count !== undefined) &&
+              (user.data[0].view_count !== undefined) ) {
+            view_count = user.data[0].view_count;
+          } else {
+            view_count = null;
+          }
+        } else {
+          id = null;
+          login = null;
+          display_name = null;
+          type = null;
+          broadcaster_type = null;
+          description = null;
+          profile_image_url = null;
+          offline_image_url = null;
+          view_count = null;
+        }
+
+
+
         const newUser = new User(
-          user.data[0].id,
-          user.data[0].login,
-          user.data[0].display_name,
-          user.data[0].type,
-          user.data[0].broadcaster_type,
-          user.data[0].description,
-          user.data[0].profile_image_url,
-          user.data[0].offline_image_url,
-          user.data[0].view_count
+          id,
+          login,
+          display_name,
+          type,
+          broadcaster_type,
+          description,
+          profile_image_url,
+          offline_image_url,
+          view_count
         );
+
         this.leaderboardUsers.set(newUser.user_id, newUser);
+
       });
     }
   }
 
   getUserLogin(user_id: string) {
     if (this.leaderboardUsers.has(user_id)) {
-      return this.leaderboardUsers.get(user_id).login;
+      if ((this.leaderboardUsers.get(user_id).login !== undefined) &&
+          (this.leaderboardUsers.get(user_id).login !== null)) {
+        return this.leaderboardUsers.get(user_id).login;
+      } else if (this.leaderboardUsers.get(user_id).login === null) {
+        return 'Deleted user';
+      } else {
+        return '';
+      }
     } else {
-      return '';
+      return 'User login';
     }
   }
 
   getUserDisplayName(user_id: string) {
     if (this.leaderboardUsers.has(user_id)) {
-      return this.leaderboardUsers.get(user_id).display_name;
+      if ((this.leaderboardUsers.get(user_id).display_name !== undefined) &&
+      (this.leaderboardUsers.get(user_id).display_name !== null)) {
+          return this.leaderboardUsers.get(user_id).display_name;
+        } else if (this.leaderboardUsers.get(user_id).display_name === null) {
+          return 'Deleted user';
+        } else {
+          return '';
+        }
     } else {
-      return '';
+      return 'Display Name';
     }
   }
 
   getUserType(user_id: string) {
     if (this.leaderboardUsers.has(user_id)) {
-      return this.leaderboardUsers.get(user_id).type;
+      if ((this.leaderboardUsers.get(user_id).type !== undefined) &&
+          (this.leaderboardUsers.get(user_id).type !== null)) {
+        return this.leaderboardUsers.get(user_id).type;
+      } else if (this.leaderboardUsers.get(user_id).type === null) {
+        return 'Deleted user';
+      } else {
+        return '';
+      }
     } else {
-      return '';
+      return 'Type';
     }
   }
 
   getUserBroadcasterType(user_id: string) {
     if (this.leaderboardUsers.has(user_id)) {
-      return this.leaderboardUsers.get(user_id).broadcaster_type;
+      if ((this.leaderboardUsers.get(user_id).broadcaster_type !== undefined) &&
+          (this.leaderboardUsers.get(user_id).broadcaster_type !== null)) {
+        return this.leaderboardUsers.get(user_id).broadcaster_type;
+      } else if (this.leaderboardUsers.get(user_id).broadcaster_type === null) {
+        return 'Deleted user';
+      } else {
+        return '';
+      }
     } else {
-      return '';
+      return 'Broadcaster type';
     }
   }
 
   getUserDescription(user_id: string) {
     if (this.leaderboardUsers.has(user_id)) {
-      return this.leaderboardUsers.get(user_id).description;
+      if ((this.leaderboardUsers.get(user_id).description !== undefined) &&
+          (this.leaderboardUsers.get(user_id).description !== null)) {
+        return this.leaderboardUsers.get(user_id).description;
+      } else if (this.leaderboardUsers.get(user_id).description === null) {
+        return 'Deleted user';
+      } else {
+        return '';
+      }
     } else {
-      return '';
+      return 'Description';
     }
   }
 
   getUserChannelUrl(user_id: string) {
     if (this.leaderboardUsers.has(user_id)) {
-      return 'https://www.twitch.tv/' + this.leaderboardUsers.get(user_id).login;
+      if ((this.leaderboardUsers.get(user_id).login !== undefined) &&
+          (this.leaderboardUsers.get(user_id).login !== null)) {
+        return this.leaderboardUsers.get(user_id).login;
+      } else if (this.leaderboardUsers.get(user_id).login === null) {
+        return 'https://www.twitch.tv/404';
+      } else {
+        return 'https://www.twitch.tv/404';
+      }
     } else {
-      return '';
+      return 'https://www.twitch.tv/404';
     }
   }
 
   getUserProfileImageUrl(user_id: string) {
     if (this.leaderboardUsers.has(user_id)) {
-      return this.leaderboardUsers.get(user_id).profile_image_url;
+      if ((this.leaderboardUsers.get(user_id).profile_image_url !== undefined) &&
+          (this.leaderboardUsers.get(user_id).profile_image_url !== null)) {
+        return this.leaderboardUsers.get(user_id).profile_image_url;
+      } else if (this.leaderboardUsers.get(user_id).profile_image_url === null) {
+        return '../../../assets/images/TwitchProfileImageTemplate.png';
+      } else {
+        return '../../../assets/images/TwitchProfileImageTemplate.png';
+      }
     } else {
-      return '';
+      return '../../../assets/images/TwitchProfileImageTemplate.png';
     }
   }
 
   getUserOfflineImageUrl(user_id: string) {
     if (this.leaderboardUsers.has(user_id)) {
-      return this.leaderboardUsers.get(user_id).offline_image_url;
+      if ((this.leaderboardUsers.get(user_id).offline_image_url !== undefined) &&
+          (this.leaderboardUsers.get(user_id).offline_image_url !== null)) {
+        return this.leaderboardUsers.get(user_id).offline_image_url;
+      } else if (this.leaderboardUsers.get(user_id).offline_image_url === null) {
+        return '../../../assets/images/TwitchOfflineImageTemplate300.png';
+      } else {
+        return '../../../assets/images/TwitchOfflineImageTemplate300.png';
+      }
     } else {
-      return '';
+      return '../../../assets/images/TwitchOfflineImageTemplate300.png';
     }
   }
 
   getUserViewCount(user_id: string) {
     if (this.leaderboardUsers.has(user_id)) {
-      return this.leaderboardUsers.get(user_id).view_count;
+      if ((this.leaderboardUsers.get(user_id).view_count !== undefined) &&
+          (this.leaderboardUsers.get(user_id).view_count !== null)) {
+        return this.leaderboardUsers.get(user_id).view_count;
+      } else if (this.leaderboardUsers.get(user_id).login === null) {
+        return 'Deleted user';
+      } else {
+        return '';
+      }
     } else {
-      return '';
+      return 0;
     }
   }
 
@@ -252,6 +411,8 @@ export class BitsComponent implements OnInit {
       return null;
     }
   }
+
+
 
 }
 
